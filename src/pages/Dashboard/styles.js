ae0e7f72e2e5749c222dfcae87cc5e8858477033
murilolemes <murilo.lemes.mhl@gmail.com>
2021-01-04 17:52:34 -0300

    panel improvements

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { shade } from 'polished';
 
 export const Container = styled.div`
   display: flex;
@@ -46,55 +45,11 @@ export const Content = styled.div`
   }
 `;
 
-export const Title = styled.h1`
-  text-align: center;
-  text-shadow: 0 2px 2px ${shade(0.2, '#ff9000')};
-
-  @media (max-width: 320px) {
-    width: 75%;
-    margin-left: 2rem;
-    font-size: 1.8rem;
-  }
-
-  @media (min-width: 321px) {
-    width: 80%;
-    margin-left: 2.1rem;
-    font-size: 2.1rem;
-  }
-
-  @media (min-width: 376px) {
-    width: 75%;
-    font-size: 2.2rem;
-  }
-
-  @media (min-width: 401px) {
-    font-size: 2.4rem;
-  }
-
-  @media (min-width: 471px) {
-    font-size: 2.6rem;
-  }
-
-  @media (min-width: 571px) {
-    font-size: 2.8rem;
-  }
-
-  @media (min-width: 671px) {
-    font-size: 3rem;
-    margin-left: 0;
-  }
-
-  @media (min-width: 769px) {
-    font-size: 3.2rem;
-    margin-bottom: 1rem;
-  }
-`;
-
 export const Body = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 1rem;
 `;
 
 export const DivGraphics = styled.div`
